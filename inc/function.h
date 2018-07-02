c7d3bfa1b88df6be8017127dfe01b36db6e890ba
EasternGD <36698746+EasternGD@users.noreply.github.com>
2018-07-02 17:42:35 +0800

    before up to ssh

--- inc/function.h
+++ inc/function.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -9,7 +11,7 @@ using namespace std;
 #define SRand() ((double)rand() / (double)RAND_MAX)
 #define BinaryRand() (rand() % 2)
 #define POPULATION_CNT 100
-#define ITERA_CNT 100
+#define ITERA_CNT 150
 // #define CROSSOVER_RATE 0.8
 // #define MUTATION_RATE 0.1
 #define PCI_decline 0.8
@@ -18,56 +20,58 @@ using namespace std;
 class DataType
 {
 private:
-  size_t pPCI = 27;
-  size_t pArea = 7;
+	size_t pPCI = 27;
+	size_t pArea = 7;
 
 public:
-  string *content;
-  size_t row;
-  size_t column;
-  double power = 0;
-  size_t budget;
-  string mustBeDone;
-  DataType(string, int);
-  virtual ~DataType() { delete[] content; }
+	string *content;
+	size_t row;
+	size_t column;
+	double power = 0;
+	int budget;
+	string mustBeDone;
+	DataType(string, int);
+	virtual ~DataType() { delete[] content; }
 };
 
 class PopulationType : public DataType
 {
 public:
-  struct ChromosomeType
-  {
-    string binaryValue;
-    size_t cost = 0;
-    double fitnessValue = 0;
-    double benefit = 0;
-    double probability = 0;
-  } BestOne;
+	struct ChromosomeType
+	{
+		string binaryValue;
+		int cost = 0;
+		double fitnessValue = 0;
+		double benefit = 0;
+		double probability = 0;
+	} BestOne;
 
-  double CROSSOVER_RATE;
-  double MUTATION_RATE;
-  double ALPHA;
-  double BETA;
-  double GAMMA;
+	double CROSSOVER_RATE;
+	double MUTATION_RATE;
+	double ALPHA;
+	double BETA;
+	double GAMMA;
 
 private:
-  ChromosomeType *parent = new ChromosomeType[POPULATION_CNT];
-  ChromosomeType *pool = new ChromosomeType[POPULATION_CNT];
-  ChromosomeType *offspring = new ChromosomeType[POPULATION_CNT];
+	ChromosomeType *parent = new ChromosomeType[POPULATION_CNT];
+	ChromosomeType *pool = new ChromosomeType[POPULATION_CNT];
+	ChromosomeType *offspring = new ChromosomeType[POPULATION_CNT];
 
 public:
-  PopulationType(string, int);
-  ~PopulationType()
-  {
-    delete[] parent;
-    delete[] pool;
-    delete[] offspring;
-  };
-  void reproduction();
-  void crossover();
-  void mutation();
-  void computeFitness(size_t);
-  void printBestOne();
+	PopulationType(string, int);
+	~PopulationType()
+	{
+		delete[] parent;
+		delete[] pool;
+		delete[] offspring;
+	};
+	void reproduction();
+	void crossover();
+	void mutation();
+	void computeFitness(size_t);
+	void printBestOne();
 };
+
+
 
 void saveJSON(const PopulationType&, const PopulationType&, string);
--- main.cpp
+++ main.cpp
@@ -2,48 +2,54 @@
 
 int main(int argc, char *argv[])
 {
-    cout << "stage 1 ---------------------" << endl;
-    srand(time(NULL));
-    PopulationType population(argv[1], 1); //1 mean for stage 1 ,2 mean for stage 2
-    PopulationType population2(argv[1], 2);
+	//cout << "stage 1 ---------------------" << endl;
+	srand(time(NULL));
+	//srand(atoi(argv[9]));
 
-    population.CROSSOVER_RATE = atof(argv[3]);
-    population2.CROSSOVER_RATE = atof(argv[3]);
-    population.MUTATION_RATE = atof(argv[4]);
-    population2.MUTATION_RATE = atof(argv[4]);
-    population.ALPHA = atof(argv[5]);
-    population2.ALPHA = atof(argv[5]);
-    population.BETA = atof(argv[6]);
-    population2.BETA = atof(argv[6]);
-    population.GAMMA = atof(argv[7]);
-    population2.GAMMA = atof(argv[7]);
-    population.budget = atoi(argv[8]);
+		PopulationType population(argv[1], 1); //1 mean for stage 1 ,2 mean for stage 2
+		PopulationType population2(argv[1], 2);
 
-    cout << "budget: " << population.budget / 1000000.0 << " M" << endl;
+		population.CROSSOVER_RATE = atof(argv[3]);
+		population2.CROSSOVER_RATE = atof(argv[3]);
+		population.MUTATION_RATE = atof(argv[4]);
+		population2.MUTATION_RATE = atof(argv[4]);
+		population.ALPHA = atof(argv[5]);
+		population2.ALPHA = atof(argv[5]);
+		population.BETA = atof(argv[6]);
+		population2.BETA = atof(argv[6]);
+		population.GAMMA = atof(argv[7]);
+		population2.GAMMA = atof(argv[7]);
+		population.budget = atoi(argv[8]);
 
-    for (size_t i = 0; i < ITERA_CNT; i++)
-    {
-        cout << "(" << i + 1 << "/" << ITERA_CNT << ")" << endl;
-        population.reproduction();
-        population.crossover();
-        population.mutation();
-        population.computeFitness(i);
-    }
-    population.printBestOne();
 
-    // cout << "\nstage 2 ---------------------" << endl;
-    // population2.budget = population.budget - population.BestOne.cost;
+		//cout << "budget: " << population.budget / 1000000.0 << " M" << endl;
 
-    // cout << "budget: " << population2.budget / 1000000.0 << " M" << endl;
+		for (size_t i = 0; i < ITERA_CNT; i++)
+		{
+			//cout << "(" << i + 1 << "/" << ITERA_CNT << ")" << endl;
+			population.reproduction();
+			population.crossover();
+			population.mutation();
+			population.computeFitness(i);
+		}
 
-    // for (size_t i = 0; i < ITERA_CNT; i++)
-    // {
-    //     cout << "(" << i + 1 << "/" << ITERA_CNT << ")" << endl;
-    //     population2.reproduction();
-    //     population2.crossover();
-    //     population2.mutation();
-    //     population2.computeFitness(i);
-    // }
-    // population2.printBestOne();
-    saveJSON(population, population2, argv[2]);
+		population.printBestOne();
+		saveJSON(population, population2, argv[2]);
+
+	return(0);
+	// cout << "\nstage 2 ---------------------" << endl;
+	// population2.budget = population.budget - population.BestOne.cost;
+
+	// cout << "budget: " << population2.budget / 1000000.0 << " M" << endl;
+
+	// for (size_t i = 0; i < ITERA_CNT; i++)
+	// {
+	//     cout << "(" << i + 1 << "/" << ITERA_CNT << ")" << endl;
+	//     population2.reproduction();
+	//     population2.crossover();
+	//     population2.mutation();
+	//     population2.computeFitness(i);
+	// }
+	// population2.printBestOne();
+
 }
